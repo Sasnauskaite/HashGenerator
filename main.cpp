cc12e76af96c692aabb256a769fcb65c6c32821b
Sasnauskaite <78842487+Sasnauskaite@users.noreply.github.com>
2021-10-07 11:34:57 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,24 +2,31 @@
 #include <fstream>
 #include <vector>
 #include <string>
-#include <cstring>
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
 #include <stdlib.h>
 #include <time.h>
+#include "sha256.cpp"
 
 using namespace std;
 
-string letters [52] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 
-"z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", 
-"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-int numbers [52] = {11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 
-99999, 11112, 22223, 33334, 44445, 55556, 66667, 77778, 88889, 99990, 11123,
-22234, 33345, 44456, 55567, 66678, 77789, 88890, 99901, 11234, 22345, 33456, 
-44567, 55678, 66789, 77890, 88901, 99012, 12345, 23456, 34567, 45678, 56789, 
-67890, 78901, 89012, 91234, 96385, 63852, 38527, 85274, 52741, 27410, 74196};
+string letters [70] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+"K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", 
+"Z", "Ą", "Č", "Ę", "Ė", "Į", "Š", "Ų", "Ū", "Ž", "a", "b", "c", "d", "e", 
+"f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+"u", "v", "w", "x", "y", "z", "ą", "č", "ę", "ė", "į", "š", "ų", "ū", "ž"};
+int numbers [70] = {1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 
+9999, 1112, 2223, 3334, 4445, 5556, 6667, 7778, 8889, 9990, 1123,
+2234, 3345, 4456, 5567, 678, 7789, 8890, 9901, 1134, 2345, 3346, 
+4567, 5578, 6689, 7780, 8801, 9912, 1245, 2346, 3567, 4578, 5789, 
+6890, 7801, 8912, 9134, 9685, 6852, 3857, 8574, 5241, 2740, 7496,
+9745, 9765, 8654, 7643, 6532, 5421, 4320, 3209, 2098, 1987, 1375, 
+8640, 5657, 8989, 5356, 2525, 6523, 1259};
+
 void txtFILE (string &info)
 {
-    ifstream in ("tekstas.txt");
+    ifstream in ("empty.txt");
     in >> info;
     in.close();
 }
@@ -45,7 +52,7 @@ string MoreThanOne ()
 }
 void isvedimas(string bye)
 {
-    cout << "Hex: " <<hex<< bye << endl; 
+    cout << "Hash: " <<hex<< bye << endl; 
 }
 void HASHING(string text, string code)
 {
@@ -84,8 +91,8 @@ void HASHING(string text, string code)
                 integer = 0;
                 integer = numbers[j+5] * INPUTsize;
                 integer = integer + ((integer+5)* (INPUTsize * (j + j + 3)));
-                //if(integer<0)
-                //    integer=integer*(-1);
+                if(integer<0)
+                    integer=integer*(-1);
                 temprorary = ' ';
                 temprorary = letters[j+1];
                 code += to_string(integer);
@@ -97,9 +104,9 @@ void HASHING(string text, string code)
     int i = 0;
     while(code.size() < 64)
     {
-        int k1 = numbers[INPUTsize+i]*(INPUTsize+i);
-        k1 = k1 % 10;
-        code += to_string(k1);
+        int change = numbers[INPUTsize+i]*(INPUTsize+i);
+        change = change % 10;
+        code += to_string(change);
         i++;
         if(i >= 52)
         {
@@ -113,18 +120,219 @@ void HASHING(string text, string code)
     }  
     isvedimas(code);
 }
-int main()
+string HASHING2(string text)
 {
+    srand((unsigned int)time(NULL));
+    string code;
+    code.clear();
+    vector <string> arr;
+    string temp, temprorary;
+    long int integer;
+    int sizes, index, n=64;
+    int INPUTsize = text.size();
+    int ii= 0;
+    if(INPUTsize > 6)
+    {
+        string::size_type i = 0;
+        while( i < text.size() && arr.size() < 6) 
+        {
+            temp = text[i];
+            arr.push_back(temp);
+            i=i+3;
+        }
+    }
+    else
+    {
+        for (string::size_type i = 0; i < text.size(); i++) 
+        {
+            temp = text[i];
+            arr.push_back(temp);
+        }
+    }
+    int m=code.size();
+    for(int i = 0; i<arr.size(); i++)
+    {
+        for(int j=0; j<52; j++)
+        {
+            if(arr[i] == letters[j])
+            {
+                integer = 0;
+                integer = numbers[j+5] * INPUTsize;
+                integer = integer + ((integer+5)* (INPUTsize * (j + j + 3)));
+                if(integer<0)
+                    integer=integer*(-1);
+                temprorary = ' ';
+                temprorary = letters[j+1];
+                code += to_string(integer);
+                code += temprorary;
+            }
+        }
+    }
+
+    int i = 0;
+    while(code.size() < 64)
+    {
+        int iSecret = rand() % 69;
+        int change = numbers[iSecret+i]*(INPUTsize+i);
+        change = change % 10;
+        if(change<0)
+        {
+            change=change*(-1);
+        }
+        code += to_string(change);//+letters[iSecret];
+        i++;
+        if(i >= 70)
+        {
+            i=0;
+        }
+    }
+    
+    while(code.size() >= 65)
+    {
+        code.erase(code.end()-1);
+    }  
+    return code;
+}
+int Kolizija10(int NumOfstrings, vector<string> poros10)
+{
+    int diff=0;
+    int SameHash10=0;
+    for(int i=0; i<NumOfstrings; i++)
+    {
+        if(poros10[i]!=poros10[i+1])
+        {
+            if(HASHING2(poros10[i])!=HASHING2(poros10[i+1]))
+            {
+                diff+=1;
+            }
+        }
+        i++;
+    }
+    cout<<diff<<"For 10"<<endl;
+    SameHash10=25000-diff;
+    return SameHash10;
+}
+void Kolizija()
+{
+    int NumOfstrings=50000;
+    vector <string> poros10, poros100, poros500, poros1000;
+    string tempp;
+    int SameHash10=0, SameHash100=0, SameHash500=0, SameHash1000=0;
+    char temp[11];
+    ifstream in ("poros10.txt");
+    for(int i=0; i<NumOfstrings; i++)
+    {
+        in.get (temp, 11);
+        in.ignore (20, '\n');
+        tempp=temp;
+        poros10.push_back(tempp);
+    }
+    in.close();
+    SameHash10=Kolizija10(NumOfstrings, poros10);
+    char t[101];
+    ifstream inf ("poros100.txt");
+    for(int i=0; i<NumOfstrings; i++)
+    {
+        inf.get (t, 101);
+        inf.ignore (20, '\n');
+        tempp=t;
+        poros100.push_back(tempp);
+    }
+    inf.close();
+    SameHash100=Kolizija10(NumOfstrings, poros100);
+    char tmp[501];
+    ifstream fin ("poros500.txt");
+    for(int i=0; i<NumOfstrings; i++)
+    {
+        fin.get (tmp, 501);
+        fin.ignore (20, '\n');
+        tempp=tmp;
+        poros500.push_back(tempp);
+    }
+    fin.close();
+    SameHash500=Kolizija10(NumOfstrings, poros500);
+    char tem[1001];
+    ifstream f ("poros1000.txt");
+    for(int i=0; i<NumOfstrings; i++)
+    {
+        f.get (tem, 1001);
+        f.ignore (20, '\n');
+        tempp=tem;
+        poros1000.push_back(tempp);
+    }
+    f.close();
+    SameHash1000=Kolizija10(NumOfstrings, poros1000);
+    cout<<"Hash kodai sutapo: \nPorose po 10: "<<SameHash10<<"\nPorose po 100: "<<SameHash100
+    <<"\nPorose po 500: "<<SameHash500<<"\nPorose po 1000: "<<SameHash1000<<"\n";
+}
+void Efektyvumas()
+{
+    vector <string> kons;
+    string temp, out;
+    ifstream in ("konstitucija.txt");
+    for(int i=0; i<774; i++)
+    {
+        getline(in, temp);
+        temp.erase(remove(temp.begin(), temp.end(), ','), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '.'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '-'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '('), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), ')'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), '/'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), ';'), temp.end());
+        temp.erase(remove(temp.begin(), temp.end(), ':'), temp.end());
+        kons.push_back(temp);
+    }
+    in.close();
+    auto start = std::chrono::steady_clock::now();
+    for(int j=0; j<774; j++)
+    {
+        HASHING2(kons[j]);
+    }
+    cout << "Failo konstitucija.txt hashavimas truko: " << 
+    chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
+}
+void Papildoma(vector <string> words)
+{
+    string input;auto start = std::chrono::steady_clock::now();
+    for(int i=0; i<100000; i++)
+    {
+        sha256(words[i]);
+    }
+    cout << "100 000 string'u hash'avimas su SHA256 truko: " << 
+    chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start).count() << " ms" << endl;
+    auto start1 = std::chrono::steady_clock::now();
+    for(int i=0; i<100000; i++)
+    {
+        HASHING2(input);
+    }
+    cout << "100 000 string'u hash'avimas su mano funkcija truko: " << 
+    chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - start1).count() << " ms" << endl;
+}
+string FileForPapildoma()
+{
+    vector <string> words;
+    string word;
+    ifstream in ("visos.txt");
+    for(int i=0; i<100000; i++)
+    {
+        in >> word;
+        words.push_back(word);
+    }
+    in.close();
+    Papildoma(words);
+}
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL, "Lithuanian");
     string in;
-    string answer;
+    string answer, answer1;
     int answer2;
     string output;
     string FileNameInput, inside;
     vector <string> comparing, FileName;
-    cout << "Kokia bus jusu ivestis? (t-tekstinio failo ar n-rankiniu budu)" << endl;
+    cout << "Kokia bus jusu ivestis? (t-tekstinio failo ar n-rankiniu budu ar l-tyrimai)" << endl;
     cin >> answer;
-    cout << "Kiek atskiru ivesciu norite lyginti? (iveskite skaiciu nuo 1 iki 5)\nJei nenorite lyginti, rasykite 1" << endl;
-    cin >> answer2;
     if(answer == "t") 
     {
         if (answer2>1 && answer2<=5)
@@ -157,7 +365,6 @@ int main()
     }
     else if(answer == "n") 
     {
-        
         if (answer2>1 && answer2<=5)
         {
             for(int i=0; i<answer2; i++)
@@ -182,11 +389,27 @@ int main()
             HASHING(in, output);
         }
     }
+    else if(answer=="l")
+    {
+        cout << "Ka norite tikrinti? \n(e-efektyvuma / k-kolizija / p-papildoma)" << endl;
+        cin >> answer1;
+        if(answer1=="e")
+        {
+            Efektyvumas();
+        }
+        else if(answer1=="k")
+        {
+            Kolizija();
+        }
+        else if(answer1=="p")
+        {
+            FileForPapildoma();
+        }
+    }
     else 
     {
         cout << "Jusu atsakymas neteisingas, paleiskite programa is naujo" ;
         exit;
-    }
-
+    }    
     return 0;
 }
